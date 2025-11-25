90a66a0bc590b2e291b296c61b797e1138e81f43
akshaychoudhary1707-cyber <akshaychoudhary1707@gmail.com>
2025-11-25 15:40:26 +0530

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,108 @@
+/*eslint-env browser*/ 
+/*eslint-disable no-console*/
+
+
+//var name = "Jack";
+//var age = 23;
+//var message = "Hi my name is " + name + " and i am " + age + " years old.";
+//console.log(message);
+
+//var sum = 23 + 27;
+//var sub = 27 - 23;
+//var mul = 27 * 23;
+//var div = 27 / 23;
+//console.log(sum)
+//console.log(sub)
+//console.log(mul)
+//console.log(div)
+
+//var var1 = 23;
+//var var2 = "23";
+//if ( var1 === var2){
+//    console.log("these are same");
+//}
+//else{
+//    console.log("not same");
+//}
+
+//var balance = 300;
+//var nikeshoes = 799.23;
+//var coupon = 500;
+//if (nikeshoes<=balance){
+//    balance -=nikeshoes;
+//    console.log("we just bought some dope shoes");
+//    console.log("account balance:- ",balance);
+//}
+//else if(nikeshoes<=balance+coupon){
+//     console.log("we just bought some dope shoes with a cupon");
+//     balance-=nikeshoes-coupon;
+//     console.log("account balance:- ",balance);
+//}
+//else{
+//    console.log("you too broke for shoes bro!");
+//}
+
+//if(1 === 1 || 2 === "2" ){
+//    console.log("You win");
+//}
+//else{
+//    console.log("You are second");
+//}
+
+//var cat1 = 5;
+//var cat2 = 10;
+//var cat3 = 1;
+//var disablehandicap = true;
+//if( cat1 > cat2 && cat1 > cat3 && !disablehandicap){
+//    console.log("cat1 is cutest");
+//}
+//else if(cat2 > cat1 && cat2 > cat3 && !disablehandicap){
+//    console.log("cat2 is the cutest");
+//}
+//else if(cat3 > cat1 && cat3 > cat2 || disablehandicap){
+//    console.log("cat3 is the cutest");
+//}
+
+//var students=["timmy","jenessa","arun"];
+//console.log(students);
+//var naughtylist = [];
+//naughtylist.push(students[0]);
+//var index = naughtylist.indexOf("timmy");
+//if(index>-1){
+//    naughtylist.splice(index,1);
+//}
+//console.log(index);
+//console.log(naughtylist);
+
+//var index = 10;
+//for (var x = 0; x < index; x++){
+//    console.log(x);
+//}
+
+var students=["john","jacob","jingle","heimer","smith"];
+for(var i=0; i<students.length; i++){
+    console.log(students[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
